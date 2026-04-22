09f17b0fdf66d19260e0cee469b40ff643c6ccd0
parkm0053 <parkm0053@my.sduhsd.net>
2026-04-22 14:15:16 -0700

    slightly more course profile

--- src/CourseProfile.jsx
+++ src/CourseProfile.jsx
@@ -1,24 +1,30 @@
 import './CourseProfile.css'
 import { useState } from 'react'
 
-function CourseProfile() {
+function CourseProfile({courseName, courseDesc, prereqs, subject, difficulty, hwTime}) {
     return(
-        <div>
+        <div id="theCourseProfileOne">
             <button>Back</button>
             <h1>{courseName}</h1>
-            <div>
-                <h3>Description</h3>
-                <p>{desc}</p>
+            <div className='boxForProfile' id='box1'>
+                <h3>Description:</h3>
+                <p>{courseDesc}</p>
             </div>
-            <div>
-                <h3>
-                    <h3>Prereqs</h3>
-                    <p>{prereqs}</p>
-                </h3>
+            <div className='boxForProfile'>
+                <h3>Prereqs:</h3>
+                <p>{prereqs}</p>
             </div>
-            <div>
-                <h3>Subject</h3>
+            <div className='boxForProfile'>
+                <h3>Subject:</h3>
                 <p>{subject}</p>
+            </div>
+            <div className='boxForProfile'>
+                <h3>Difficulty:</h3>
+                <p>{difficulty}</p>
+            </div>
+            <div className='boxForProfile'>
+                <h3>Average time on HW:</h3>
+                <p>{hwTime}</p>
             </div>
         </div>
     )
