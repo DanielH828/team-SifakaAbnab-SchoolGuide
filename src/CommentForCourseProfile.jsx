1312cee62efe286470c93e13eb3c001868f0001b
parkm0053 <parkm0053@my.sduhsd.net>
2026-04-24 13:53:39 -0700

    course profile and comments are formatted and working yipeee

--- src/CommentForCourseProfile.jsx
+++ src/CommentForCourseProfile.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
+import './CommentForCourseProfile.css'
 import ProfilePicture from './assets/ProfilePicture.svg'
 
 function CommentForCourseProfile({name, reviewedDiff, reviewedWorkload, review}) {
     return(
-        <div>
-            <img src={ProfilePicture}/>
-            <p>{name}</p>
-            <p></p>
-            <p>Difficulty: {reviewedDiff}</p>
-            <p>Workload: {reviewedWorkload}</p>
-            <p>{review}</p>
+        <div id="commentBox">
+            <div id="commentHeader">
+                <div id="commentOnTheLeft">
+                    <img src={ProfilePicture} id="commentPicture"/>
+                    <p id="commentName">{name}</p>
+                </div>
+                <div id="commentOnTheRight">
+                    <p id="commentDiff">Difficulty: {reviewedDiff}</p>
+                    <p id="commentWorkload">Workload: {reviewedWorkload}</p>
+                </div>
+            </div>
+            <p id="commentReview">{review}</p>
         </div>
     )
 }
--- src/CourseProfile.jsx
+++ src/CourseProfile.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 function CourseProfile({courseName, courseDesc, prereqs, subject, difficulty, hwTime, teachers}) {
     return(
+        <div>
         <div id="theCourseProfileOne">
             <button id="backButton">Back</button>
 
@@ -24,6 +25,11 @@ function CourseProfile({courseName, courseDesc, prereqs, subject, difficulty, hw
                 <p>{subject}</p>
             </div>
 
+            <div className='boxForProfile' id='boxTeachers'>
+                <h3>Teachers:</h3>
+                <p>{teachers}</p>
+            </div>
+
             <div className='boxForProfile' id='boxDifficulty'>
                 <h3>Difficulty:</h3>
                 <p>{difficulty}</p>
@@ -34,24 +40,26 @@ function CourseProfile({courseName, courseDesc, prereqs, subject, difficulty, hw
                 <p>{hwTime}</p>
             </div>
 
-            <div className='boxForProfile' id='boxTeachers'>
-                <h3>Teachers:</h3>
-                <p>{teachers}</p>
-            </div>
-
             <div id="whyCantIGethrToWork"></div>
             
             <h2>Reviews</h2>
 
             <p id="boxSubRating">Overall Ratings:</p>
-            <p id="boxSubDifficulty">Difficulty: {difficulty}</p>
+            <p id="boxSubDifficulty">Diff: {difficulty}</p>
             <p id="boxSubWorkload">Workload: {hwTime}</p>
             <button id="boxSubReviews">Add Review</button>
+        </div>
             <CommentForCourseProfile 
                 name="Danny He"
                 reviewedDiff="1/10"
                 reviewedWorkload="0 Mins"
                 review="This was a very cool class. lksadjflsajflksajf"
+            />
+            <CommentForCourseProfile 
+                name="Tyler Levoe"
+                reviewedDiff="3/10"
+                reviewedWorkload="0 Mins"
+                review="this is like the bst class in the computer science classes its really good and i like that. i appreciate this class for being such a positive and good class. thankn fo making this class thanks"
             />
         </div>
     )
