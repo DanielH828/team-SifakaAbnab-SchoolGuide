f68a55134d0556abd3448adba70f9d80dda97b8c
qawsedrfgyhujikolpmnbvcxz <gudurua5933@my.sduhsd.net>
2026-04-27 13:38:55 -0700

    created the top bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import heroImg from './assets/hero.png'
 import CourseProfile from './CourseProfile.jsx'
 import CourseList from './CourseList.jsx'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +14,7 @@ function App() {
 
   return (
     <>
+      <Navbar></Navbar>
       <CourseProfile 
         setPage={setPage}
         courseName="Human Computer Interactions" 
